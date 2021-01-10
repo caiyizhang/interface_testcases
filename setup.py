08c6a3a5e90f89c0dba27ba0872748ccc4906846
caiyizhang <caiyizhang@kuaishou.com>
2021-01-10 20:20:56 +0800

    修改setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="接口测试用例生成",
-    version="0.0.1",
+    name="interfacecases",
+    version="0.1.0",
     author="Caiyizhang",
     author_email="1031282751@qq.com",
     description="提供接口测试用例生成方法",
